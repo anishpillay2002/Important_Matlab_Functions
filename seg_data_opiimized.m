3588500a437e30f58edeeba563ca950dca65ea60
anishpillay2002 <anishpillay2002@gmail.com>
2017-04-06 23:10:08 -0500

    Add files via upload

--- seg_data_opiimized.m
+++ seg_data_opiimized.m
@@ -7,13 +7,14 @@ left=[];
 
 codes=[1,5,8];
 
-forj=1:4
+for j=1:4
 for i=1:length(codes)
 code=codes(i);
 data_initial=[]
 data_initial=data(data(:,nextid)==code,:);
+if ~isempty(data_initial)==1
 [nextid,left,Time_data,Vel_data,LZH_data] = seg_data (data_intial,nextid,code,left,Time_data,Vel_data,LZH_data)
-
+end
 
 
 end
