fe189bb8c1a82e1764c658a5f3ff75a96e1aa3c2
anishpillay2002 <anishpillay2002@gmail.com>
2017-04-07 09:24:49 -0500

    Add files via upload

--- seg_data.m
+++ seg_data.m
@@ -1,28 +1,29 @@
-## Copyright (C) 2017 anish_4s67ibh
-## 
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% ## Copyright (C) 2017 anish_4s67ibh
+% ## 
+% ## This program is free software; you can redistribute it and/or modify it
+% ## under the terms of the GNU General Public License as published by
+% ## the Free Software Foundation; either version 3 of the License, or
+% ## (at your option) any later version.
+% ## 
+% ## This program is distributed in the hope that it will be useful,
+% ## but WITHOUT ANY WARRANTY; without even the implied warranty of
+% ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% ## GNU General Public License for more details.
+% ## 
+% ## You should have received a copy of the GNU General Public License
+% ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% 
+% ## -*- texinfo -*- 
+% ## @deftypefn {Function File} {@var{retval} =} seg_data (@var{input1}, @var{input2})
+% ##
+% ## @seealso{}
+% ## @end deftypefn
+% 
+% ## Author: anish_4s67ibh <anish_4s67ibh@ANISH_PC>
+% ## Created: 2017-04-06
 
-## -*- texinfo -*- 
-## @deftypefn {Function File} {@var{retval} =} seg_data (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
+function [nextid,left,Time_data,Vel_data,LZH_data,LLH_data] = seg_data (data,nextid,code,left,Time_data,Vel_data,LZH_data,LLH_data)
 
-## Author: anish_4s67ibh <anish_4s67ibh@ANISH_PC>
-## Created: 2017-04-06
-
-function [nextid,left,Time_data,Vel_data,LZH_data] = seg_data (data,nextid,code,left,Time_data,Vel_data,LZH_data)
 remain=[];
 slice=[];
 sz_loc=nextid+1;
@@ -34,14 +35,15 @@ end_loc=strt_loc+sz-1;
 slice=data(data(:,sz_loc)==sz,[1,strt_loc:end_loc]);
 remain=data(data(:,sz_loc)==sz,[1,end_loc+1:end]);
 left=padcat(left,remain);
-nextid=2;
+%nextid=2;
 if code==1
 Time_data=padcat(Time_data,slice);
 elseif code==8
 Vel_data=padcat(Vel_data,slice);
 elseif code==5
 LZH_data=padcat(LZH_data,slice);
+elseif code==2
+LLH_data=padcat(LLH_data,slice);
 end
 end
-
-endfunction
+end
--- seg_data_opiimized.m
+++ seg_data_opiimized.m
@@ -1,11 +1,11 @@
-nextid=2;
+nextid=13;
 Time_data=[];
 Vel_data=[];
 LZH_data=[];
-
-data=back_data;
-
-codes=[1,5,8];
+LLH_data=[];
+data=testing_out;
+%left=[]
+codes=[1,5,8,2];
 
 for j=1:4
 left=[];
@@ -14,11 +14,12 @@ code=codes(i);
 data_initial=[];
 data_initial=data(data(:,nextid)==code,:);
 if ~isempty(data_initial)==1
-[nextid,left,Time_data,Vel_data,LZH_data] = seg_data (data_initial,nextid,code,left,Time_data,Vel_data,LZH_data)
+[nextid,left,Time_data,Vel_data,LZH_data,LLH_data] = seg_data (data_initial,nextid,code,left,Time_data,Vel_data,LZH_data,LLH_data);
 end
 
 
 end
 data=left;
+nextid=2;
 
 end
